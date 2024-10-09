bd4c68f54646273b208de8e4c4ca5edeaf0a7313
Carmen Rubio Garcia <59648070+crubiogarcia@users.noreply.github.com>
2024-10-09 20:39:40 +0200

    Add files via upload

---
+++ Curve/CleanPolyAngleGreaterThanComponent.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Moth
+{
+    public class CleanPolyAngleGreaterThanComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the CleanPolyline class.
+        /// </summary>
+        public CleanPolyAngleGreaterThanComponent()
+          : base("Clean Poly Angle Greater", "CPoly G",
+              "Reduces segments in a polyline where the angle between consecutive segments is greater than the specified tolerance",
+              "Moth", "Curve")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Polyline", "P", "Polyline", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Angle", "a", "Angle tolerance in radians", GH_ParamAccess.item, 2.00);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Polyline", "R", "Reduced polyline", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //Inputs
+            Rhino.Geometry.Curve C = null;
+            DA.GetData(0, ref C);
+            double a = 0.15;
+            DA.GetData(1, ref a);
+
+            //Algorithm
+
+            // Convert polyline points to a list
+
+            Polyline P;
+            if (C.TryGetPolyline(out P))
+            {
+                // The curve was successfully converted to a polyline
+            }
+            else
+            {
+                this.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "P must be a polyline");
+                return;
+            }
+
+
+            List<Point3d> pts = new List<Point3d>(P.ToArray());
+
+
+
+            // Initialize a flag to track if any point was removed in the last iteration
+            bool pointRemoved = true;
+
+            // Continue looping while points are being removed
+            while (pointRemoved)
+            {
+                pointRemoved = false;
+
+                // Iterate through the points and check angles between consecutive segments
+                for (int i = 1; i < pts.Count - 1; i++)
+                {
+                    Vector3d vec1 = pts[i] - pts[i - 1];
+                    Vector3d vec2 = pts[i + 1] - pts[i];
+
+                    double angle = Vector3d.VectorAngle(vec1, vec2);
+
+                    // Remove the point if the angle is less than the threshold
+                    if (angle > a)
+                    {
+                        pts.RemoveAt(i);
+                        pointRemoved = true;
+                        break; // Exit the loop to restart checking from the beginning
+                    }
+                }
+            }
+
+            // Check if the polyline is closed
+            if (pts.Count > 2 && P.IsClosed)
+            {
+                // Calculate the angle between the last and first segments
+                Vector3d lastSegment = pts[pts.Count - 1] - pts[pts.Count - 2];
+                Vector3d firstSegment = pts[1] - pts[0];
+
+                double angle = Vector3d.VectorAngle(lastSegment, firstSegment);
+
+                // Remove both the first and last points if the angle is greater than the threshold
+                if (angle > a)
+                {
+                    pts.RemoveAt(pts.Count - 1); // Remove last point
+                    pts.RemoveAt(0); // Remove first point
+                    pointRemoved = true;
+                }
+            }
+
+            // Create a new polyline from the filtered points
+            Polyline filteredPolyline = new Polyline(pts);
+
+
+            if (P.IsClosed && !filteredPolyline.IsClosed)
+            {
+                pts.Add(pts[0]);
+            }
+
+            Polyline CRV = new Polyline(pts);
+
+            //Output
+            DA.SetData(0, CRV);
+
+        }
+
+        //Set exposure level
+        public override GH_Exposure Exposure => base.Exposure;
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                var ImageBytes = Moth.Properties.Resources.CleanPolylineGreater;
+                using (MemoryStream ms = new MemoryStream(ImageBytes))
+                {
+                    System.Drawing.Bitmap image = new System.Drawing.Bitmap(ms);
+                    return image;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("d6b3afc2-5402-4897-9e29-6a37e19874a1"); }
+        }
+    }
+}
---
+++ Curve/MultiSplitComponent.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Rhino.Geometry.Intersect;
+
+namespace Moth.Curves
+{
+    public class MultiSplitComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MultiTrimComponent class.
+        /// </summary>
+        public MultiSplitComponent()
+          : base("MultiSplit", "MSplit",
+              "Splits a list of curves based on their intersections",
+              "Moth", "Curve")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curves", "C", "List of curves to split", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Splitted Curves", "S", "List of splitted curves", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Get input curves
+            List<Rhino.Geometry.Curve> curves = new List<Rhino.Geometry.Curve>();
+            if (!DA.GetDataList(0, curves)) return;
+
+            //Algorithm
+
+            // Store intersection parameters for each curve
+            List<List<double>> splitParameters = new List<List<double>>();
+
+            // Initialize split parameters for each curve
+            for (int i = 0; i < curves.Count; i++)
+            {
+                splitParameters.Add(new List<double>());
+            }
+
+            // Find intersections between all pairs of curves
+            for (int i = 0; i < curves.Count; i++)
+            {
+                for (int j = 0; j < curves.Count; j++)
+                {
+                    if (i != j) // Ensure the same curve is not intersected with itself
+                    {
+                        var intersectionEvents = Intersection.CurveCurve(curves[i], curves[j], 0.001, 0.001);
+
+                        if (intersectionEvents != null && intersectionEvents.Count > 0)
+                        {
+                            foreach (var intersection in intersectionEvents)
+                            {
+                                // Add the intersection parameters for both curves
+                                splitParameters[i].Add(intersection.ParameterA);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // List to store all split segments
+            List<Rhino.Geometry.Curve> allSplitSegments = new List<Rhino.Geometry.Curve>();
+
+            // Split each curve at its collected intersection points
+            for (int i = 0; i < curves.Count; i++)
+            {
+                var splitSegments = curves[i].Split(splitParameters[i]);
+                if (splitSegments != null && splitSegments.Length > 0)
+                {
+                    allSplitSegments.AddRange(splitSegments);
+                }
+                else
+                {
+                    allSplitSegments.Add(curves[i]); // Add original curve if no splits
+                }
+            }
+
+            // Output the split segments
+            DA.SetDataList(0, allSplitSegments);
+        }
+    
+
+        //Set exposure level
+        public override GH_Exposure Exposure => base.Exposure;
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                var ImageBytes = Moth.Properties.Resources.MultiSplit;
+                using (MemoryStream ms = new MemoryStream(ImageBytes))
+                {
+                    System.Drawing.Bitmap image = new System.Drawing.Bitmap(ms);
+                    return image;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("59F6E920-C3DA-40C8-9922-C9619F504C2E"); }
+        }
+    }
+}
